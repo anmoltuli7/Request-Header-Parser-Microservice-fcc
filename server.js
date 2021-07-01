4df2c2ca92533940033584646766043be610dfc3
anmoltuli7 <you@example.com>
2021-07-01 09:31:09 +0530

    Header Parser 1.0.0

--- server.js
+++ server.js
@@ -19,15 +19,17 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+app.get("/api/whoami", (req, res) =>{
+  res.json({ipaddress: (req.connection.remoteAddress), language: req.headers['accept-language'], software: req.headers['user-agent']})
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
